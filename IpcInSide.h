c3a24ff8495e2d917b3ffb3f85c7f65faa733d7e
LYingSiMon <3290502915@qq.com>
2023-01-14 19:49:51 +0800

    add some function

--- IpcInSide.h
+++ IpcInSide.h
@@ -5,3 +5,17 @@ void IpcInSide_CreateNamedPipe();
 void IpcInSide_BroadcastSystemMessage();
 
 void IpcInSide_SendMessage();
+
+void IpcInSide_Mutex();
+
+void IpcInSide_Semaphore();
+
+void IpcInSide_Event();
+
+void IpcInSide_ApcPort();
+
+void IpcInSide_Session();
+
+void IpcInSide_DataSeg();
+
+
--- IpcOutSide.h
+++ IpcOutSide.h
@@ -1,1 +1,11 @@
 #pragma once
+
+void IpcOutSide_Reg();
+
+void IpcOutSide_File();
+
+void IpcOutSide_Env();
+
+void IpcOutSide_TcpUdpTable();
+
+void IpcOutSide_Atom();
